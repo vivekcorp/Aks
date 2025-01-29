9a0c2ddd9e7dc0385c11e19766d304d5c374abbc
vivekcorp <95239073+vivekcorp@users.noreply.github.com>
2025-01-29 22:49:47 +0530

    Add files via upload

---
+++ Infra/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.4.0, < 2.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.11.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    #subscription_id = "c2c9d8e4-cef1-4f3c-bba4-113f5fc55fc7"
+ features {
+   
+ }
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# vivek-infra-testing
